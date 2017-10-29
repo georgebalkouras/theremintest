5f40426592a7c547c23fa84a0bbf9872cea76a8c
George Balkouras <georgebalkouras@gmail.com>
2017-10-29 16:59:37 -0400

    test

---
+++ theremin.js
@@ -0,0 +1,45 @@
+var context = new AudioContext(),
+ 	mousedown = false,
+ 	oscillator;
+ 	
+
+ var calculateFrequency = function (mouseXPosition){
+ 	var minFrequency = 20;
+ 		maxFrequency = 2000;
+
+ 	return ((mouseXPosition / window.innerWidth) * maxFrequency) + minFrequency;
+ };
+
+ var calculateGain = function (mouseYPosition){
+ 	var minGain = 0;
+ 		maxGain = 1;
+
+ 	return 1 - ((mouseYPosition / window.innerHeight) * maxGain) + minGain;
+ };
+
+ var gainNode = context.createGain();
+
+
+
+  document.body.addEventListener('mousedown', function (e) {
+  mousedown = true;
+  oscillator = context.createOscillator();
+  oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.01);
+  gainNode.gain.setTargetAtTime(calculateGain(e.clientY), context.currentTime, 0.01);
+  oscillator.connect(gainNode);
+  gainNode.connect(context.destination);
+  oscillator.start(context.currentTime);
+  });
+
+  document.body.addEventListener('mouseup', function () {
+    mousedown = false;
+    oscillator.stop(context.currentTime);
+    oscillator.disconnect();
+  });
+
+  document.body.addEventListener('mousemove', function(e){
+  	if (mousedown){
+  		oscillator.frequency.setTargetAtTime(calculateFrequency(e.clientX), context.currentTime, 0.01);
+  		gainNode.gain.setTargetAtTime(calculateGain(e.clientY), context.currentTime, 0.01);
+  	}
+  });
